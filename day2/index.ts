fc14b6308b240ca500dd9e09b23d4499a18f95b1
Yadullah Duman <yadullah.duman@gmail.com>
2023-12-26 21:25:18 +0100

    finish day 2

--- day2/index.ts
+++ day2/index.ts
@@ -7,6 +7,7 @@ const G_MAX = 13;
 const B_MAX = 14;
 
 type Color = "red" | "green" | "blue";
+type Grab = { count: number; color: Color; ok?: boolean };
 
 function isOk(count: number, color: Color) {
   switch (color) {
@@ -19,7 +20,13 @@ function isOk(count: number, color: Color) {
   }
 }
 
-(async function solve() {
+function getCountsByColor(gameplay: Grab[][], color: Color) {
+  return gameplay.flatMap((play) =>
+    play.filter((p) => p.color === color).map((p) => p.count),
+  );
+}
+
+async function solvePart1() {
   const rl = readline.createInterface({
     input: fs.createReadStream("./input.txt"),
     crlfDelay: Infinity,
@@ -35,14 +42,14 @@ function isOk(count: number, color: Color) {
       .split(";")
       .map((s) => s.trim())
       .map((s) => {
-        return s.match(regex)?.map((m) => {
+        return s.match(regex)!.map((m) => {
           const [count, color] = m.split(" ");
           const countAsNum = parseInt(count);
           return {
             count: countAsNum,
             color,
             ok: isOk(countAsNum, color as Color),
-          };
+          } as Grab;
         });
       });
 
@@ -53,4 +60,50 @@ function isOk(count: number, color: Color) {
 
   await events.once(rl, "close");
   console.log(`Result: ${acc}`);
+}
+
+async function solvePart2() {
+  const rl = readline.createInterface({
+    input: fs.createReadStream("./input.txt"),
+    crlfDelay: Infinity,
+  });
+
+  let acc = 0;
+  const regex = /(\d+)\s+(\w+)/g;
+
+  rl.on("line", (line) => {
+    const game = parseInt(line.split(":")[0].split(" ")[1]);
+    const gameplay = line
+      .split(":")[1]
+      .split(";")
+      .map((s) => s.trim())
+      .map((s) => {
+        return s.match(regex)!.map((m) => {
+          const [count, color] = m.split(" ");
+          const countAsNum = parseInt(count);
+          return {
+            count: countAsNum,
+            color: color as Color,
+          } as Grab;
+        });
+      });
+
+    const reds = getCountsByColor(gameplay, "red");
+    const greens = getCountsByColor(gameplay, "green");
+    const blues = getCountsByColor(gameplay, "blue");
+
+    const maxRed = Math.max(...reds);
+    const maxGreen = Math.max(...greens);
+    const maxBlues = Math.max(...blues);
+
+    acc += maxRed * maxGreen * maxBlues;
+  });
+
+  await events.once(rl, "close");
+  console.log(`Result: ${acc}`);
+}
+
+(async function solve() {
+  // await solvePart1();
+  await solvePart2();
 })();
