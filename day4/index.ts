16af6ea6c473624d9cba3601039eaa1d06ec143a
Yadullah Duman <yadullah.duman@gmail.com>
2024-01-01 04:21:57 +0100

    finish day 4

--- day4/index.ts
+++ day4/index.ts
@@ -2,13 +2,17 @@ import fs from "node:fs";
 import events from "events";
 import readline from "readline";
 
-async function solvePart1() {
+type Card = { left: number[]; right: number[] };
+const CARDS: Card[] = [];
+
+type CardMatch = [number, number];
+
+async function parseInput() {
   const rl = readline.createInterface({
     input: fs.createReadStream("./input.txt"),
     crlfDelay: Infinity,
   });
 
-  const matches: number[] = [];
   rl.on("line", line => {
     const card = line
       .split(":")[1]
@@ -18,19 +22,18 @@ async function solvePart1() {
       .map(l => l.filter(x => x))
       .map(l => l.map(Number));
 
-    const left = card[0];
-    const right = card[1];
-
-    let match = 0;
-    for (const n of right) {
-      if (left.includes(n)) {
-        match++;
-      }
-    }
-
-    matches.push(match);
+    CARDS.push({ left: card[0], right: card[1] });
   });
   await events.once(rl, "close");
+}
+
+async function solvePart1() {
+  const matches: number[] = [];
+
+  CARDS.forEach(card => {
+    matches.push(card.right.filter(n => card.left.includes(n)).length);
+  });
+
   console.log(
     matches
       .filter(n => n !== 0)
@@ -39,6 +42,28 @@ async function solvePart1() {
   );
 }
 
+async function solvePart2() {
+  const scratchCards: number[] = new Array(CARDS.length).fill(1);
+
+  CARDS.forEach((card, idx) => {
+    const matchCount = card.right.filter(n => card.left.includes(n)).length;
+
+    if (matchCount) {
+      for (let i = idx + 1; i < idx + 1 + matchCount; i++) {
+        if (scratchCards[i]) {
+          scratchCards[i] += scratchCards[idx] || 0;
+        }
+      }
+    }
+  });
+
+  console.log(scratchCards.reduce((acc, curr) => acc + curr, 0));
+}
+
 (async function main() {
+  await parseInput();
+  console.log("==== Solution Part 1 ====");
   await solvePart1();
+  console.log("==== Solution Part 2 ====");
+  await solvePart2();
 })();
